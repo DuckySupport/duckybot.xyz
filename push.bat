037e2082229bf1b6c1926dd35aaa77dc331d7303
bobbibones <156019296+bobbibones0@users.noreply.github.com>
2024-12-17 15:59:45 +0100

    e

--- push.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo Pushing to GitHub repository: NickIsADev/duckybot.xyz
-git add .
-git commit -m "%*"
-git push origin main
-echo Pushed changes to GitHub repository: NickIsADev/duckybot.xyz
