9cfe21aa4947531248329b6e8b8e92fd081ec4e7
bobbibones <156019296+bobbibones0@users.noreply.github.com>
2025-08-25 11:51:47 +0200

    Update panel.lua

--- servers/panel.lua
+++ servers/panel.lua
@@ -200,17 +200,19 @@ if cookie then
 
                         local editHTML = [[
                             <div class="p-4">
-                                <h3 class="text-lg font-medium text-white">Edit punishment reason...</h3>
+                                <div class="flex justify-between items-center">
+                                    <h3 class="text-lg font-medium text-white">Edit punishment reason...</h3>
+                                    <div class="flex items-center space-x-2">
+                                        <button class="p-2 rounded-full hover:bg-white/10" data-action="cancel-edit">
+                                            <img src="/images/icons/Fail.svg" class="w-5 h-5">
+                                        </button>
+                                        <button class="p-2 rounded-full hover:bg-white/10" data-action="submit-edit">
+                                            <img src="/images/icons/Success.svg" class="w-5 h-5">
+                                        </button>
+                                    </div>
+                                </div>
                                 <div class="mt-4">
                                     <input type="text" class="w-full bg-dark/50 border border-white/20 rounded-md px-3 py-2 text-white focus:outline-none focus:ring-2 focus:ring-primary" placeholder="Enter new reason">
-                                </div>
-                                <div class="mt-4 flex justify-end space-x-2">
-                                    <button class="p-2 rounded-full hover:bg-white/10" data-action="cancel-edit">
-                                        <img src="/images/icons/Fail.svg" class="w-5 h-5">
-                                    </button>
-                                    <button class="p-2 rounded-full hover:bg-white/10" data-action="submit-edit">
-                                        <img src="/images/icons/Success.svg" class="w-5 h-5">
-                                    </button>
                                 </div>
                             </div>
                         ]]
