13967a24af7f38f3b6d84b94fb7f8915f1c6d1df
bobbibones <156019296+bobbibones0@users.noreply.github.com>
2025-08-30 10:53:42 +0200

    Update csp.ts

--- netlify/edge-functions/csp.ts
+++ netlify/edge-functions/csp.ts
@@ -7,15 +7,15 @@ export default async (request: Request, context: any) => {
   newHeaders.set(
     "Content-Security-Policy",
     `
-      default-src 'self';
-      script-src 'nonce-${nonce}' 'strict-dynamic' 'self' https://unpkg.com;
-      style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-      font-src 'self' https://fonts.gstatic.com;
-      img-src 'self' data:;
-      object-src 'none';
-      base-uri 'self';
-      report-uri /.netlify/functions/__csp-violations
-    `.replace(/\s+/g, " ")
+    default-src 'self';
+    script-src 'nonce-${nonce}' 'self' https://cdn.tailwindcss.com https://unpkg.com;
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net;
+    font-src 'self' https://fonts.gstatic.com;
+    img-src 'self' data:;
+    object-src 'none';
+    base-uri 'self';
+    report-uri /.netlify/functions/__csp-violations
+  `.replace(/\s+/g, " ")
   );
 
   const bodyText = await response.text();
