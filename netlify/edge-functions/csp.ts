ad2511bb367632a08cc8e0ebd9d1c331545ac516
bobbibones <156019296+bobbibones0@users.noreply.github.com>
2025-08-30 11:02:55 +0200

    Update csp.ts

--- netlify/edge-functions/csp.ts
+++ netlify/edge-functions/csp.ts
@@ -3,23 +3,24 @@ export default async (request: Request, context: any) => {
 
   const response = await context.next();
 
+  const bodyText = await response.text();
+  const newBody = bodyText.replace(/{{nonce}}/g, nonce);
+
   const newHeaders = new Headers(response.headers);
   newHeaders.set(
     "Content-Security-Policy",
     `
-    default-src 'self';
-    script-src 'nonce-${nonce}' 'self' https://cdn.tailwindcss.com https://unpkg.com;
-    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net;
-    font-src 'self' https://fonts.gstatic.com;
-    img-src 'self' data:;
-    object-src 'none';
-    base-uri 'self';
-    report-uri /.netlify/functions/__csp-violations
-  `.replace(/\s+/g, " ")
+      default-src 'self';
+      script-src 'nonce-${nonce}' 'self' 'unsafe-eval' https://cdn.tailwindcss.com https://unpkg.com;
+      style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net;
+      font-src 'self' https://fonts.gstatic.com https://cdn.jsdelivr.net;
+      img-src 'self' data: https://cdn.discordapp.com;
+      connect-src 'self' https://api.duckybot.xyz https://devapi.duckybot.xyz;
+      object-src 'none';
+      base-uri 'self';
+      report-uri /.netlify/functions/__csp-violations
+    `.replace(/\s+/g, " ")
   );
-
-  const bodyText = await response.text();
-  const newBody = bodyText.replace("{{nonce}}", nonce);
 
   return new Response(newBody, {
     status: response.status,
