3e1c3a727b2bfeb4c3b1fc386f8c3c39af80535d
bobbibones <156019296+bobbibones0@users.noreply.github.com>
2025-10-22 19:07:31 +0200

    Update index.lua

--- index.lua
+++ index.lua
@@ -437,7 +437,7 @@ coroutine.wrap(function()
 							utils.loading()
 							if elements.servers.container then elements.servers.container.classList:remove("hidden") end
 						else
-							utils.loading("fail", "API Error", (response and response.data and response.data.message) or "An unknown error occurred. Please try again later.")
+							utils.loading("fail", "API Error", (response and response.message) or "An unknown error occurred. Please try again later.")
 						end
 					end, "GET", "https://devapi.duckybot.xyz/guilds", {
 						["Discord-Code"] = cookie
