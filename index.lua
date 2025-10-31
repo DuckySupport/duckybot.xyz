3917645e2fcae28bc6622b0eb46ef0bb2740a9dd
Troptop <87533146+NickIsADev@users.noreply.github.com>
2025-10-31 15:18:33 -0400

    update purchase popup content

--- index.lua
+++ index.lua
@@ -186,10 +186,10 @@ coroutine.wrap(function()
 					content = [[
 						<ol class="list-decimal list-inside text-white/80 space-y-2">
 							<li><a target="_blank" href="/invite" class="font-bold text-blue-400">Invite Ducky</a> to your server.</li>
-							<li>Use the <code class="bg-black/20 px-1.5 py-1 rounded">/plus manage</code> command.</li>
-							<li>Click the "Purchase Slot" button.</li>
+							<li>Use the <code class="bg-black/20 font-bold px-1.5 py-1 rounded">/plus manage</code> command.</li>
+							<li>Press the <b>Purchase Slot</b> button.</li>
 							<li>Follow the instructions to complete your purchase.</li>
-							<li>Apply the slot to a server with the <b>Use Slot</b> button.</li>
+							<li>Apply the slot to a server by pressing the <b>Use Slot</b> button.</li>
 						</ol>
 					]],
 					blur = true
