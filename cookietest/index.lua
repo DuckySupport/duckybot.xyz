34b78e636f74a450a7331ced10963e87fc593485
bobbibones <156019296+bobbibones0@users.noreply.github.com>
2026-01-30 17:47:39 +0100

    hm

---
+++ cookietest/index.lua
@@ -0,0 +1,11 @@
+package.path = "/deps/?.lua;" .. package.path
+local js = require("js")
+local http = require("http")
+local time = require("time")
+local utils = require("utils")
+
+local global = js.global
+local document = global.document
+local window = global.window
+
+utils.cookie("test", "test")
